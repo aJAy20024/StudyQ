adf7e87ebfa6f182aa15473fe1eb35b688498acc
aJAy20024 <singhjasjeet14050@gmail.com>
2026-09-17 18:52:25 +0530

    Update authRoutes.js

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const user = require("./User");
+const User = require("./user");
 const auth = require("./authMiddleware");
 const { applyStreak } = require("./streak");
 
